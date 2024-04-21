19ec1fa194661c4b0bca51aee0c8cb443100f82b
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-04-21 18:53:26 +0100

    homepage lint error

--- src/app/_components/home/Hero.tsx
+++ src/app/_components/home/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
                 <h1 className="mb-5 text-5xl font-bold text-base-200">Hello there</h1>
-                <p className="mb-5 text-blue-200">Stories is where imagination meets community. Build your story or join forces with others - it's free and open-source!</p>
+                <p className="mb-5 text-blue-200">Stories is where imagination meets community. Build your story or join forces with others - it is free and open-source!</p>
 
                 <button className="btn btn-primary">Get Started</button>
             </div>
